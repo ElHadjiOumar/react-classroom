6dea53fa2b23981e42331a12a22918e6a408c82c
El Hadji Oumar Mbengue <elhadjimbengueo@gmail.com>
2021-04-27 23:01:02 +0000

    First Commit

--- src/components/Classroom.js
+++ src/components/Classroom.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react'
 import Student from './Student'
 
 export default class Classroom extends Component {
+    constructor(){
+        super();
+        this.state = {
+            students : [
+                {id:1, nom:"EL Hadji Oumar MBENGUE"},
+                {id:2, nom:"Marie Therese NANASTA"},
+                {id:3, nom:"Wilfried PODA"},
+                {id:4, nom:"Rachid PITROIPA"},
+            ]
+        }
+    }
+
     render() {
+        const leaners = this.state.students;
         return (
             <div>
                 <h1>Liste des étudiants de {this.props.nom}</h1>
                 <ul>
-                    <Student nom="Fatou Dia" />
-                    <Student nom="Saliou Diouf" />
-                    <Student nom="Alain Gomis" />
+                    {
+                        leaners.map(leaner => <Student 
+                        key={leaner.id}
+                        nom={leaner.nom}
+                        />)
+                    }
                 </ul>
             </div>
         )
